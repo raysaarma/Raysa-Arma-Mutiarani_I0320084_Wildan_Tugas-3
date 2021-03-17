3d80ee7e0a5f6f1c377d9aaaf4ba240a6ae9635f
Raysa Arma Mutiarani <raysaarma@gmail.com>
2021-03-17 07:32:06 +0700

    up soal 1 tugas 3

--- I0320084_soal1_tugas3.py
+++ I0320084_soal1_tugas3.py
@@ -5,7 +5,6 @@ print("List Indeks 4,6,7 :", list_nama[4], list_nama[6], list_nama[7])
 list_nama[3] = "Aji"
 list_nama[5] = "Wanda"
 list_nama[9] = "Hani"
-list_nama.append("Yola")
-list_nama.append("Yuki")
+list_nama.extend(["Yola", "Yuki"])
 print(list_nama * 2)
 print(len(list_nama))
