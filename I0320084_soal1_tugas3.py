e79b37e966aa7a10c373a07d312ecbb6ae09d5ae
Raysa Arma Mutiarani <raysaarma@gmail.com>
2021-03-17 09:02:53 +0700

    up soal 1 tugas 3

--- I0320084_soal1_tugas3.py
+++ I0320084_soal1_tugas3.py
@@ -1,7 +1,7 @@
 #LIST NAMA TEMAN
 
 list_nama = ["Stefany", "Funny", "Alya", "Diva", "Sekar", "Zaneta", "Narista" ,"Rifqi", "Utsman", "Maurich"]
-print("List Indeks 4,6,7 :", list_nama[4], list_nama[6], list_nama[7])
+print("List Indeks 4,6,7 :", list_nama[4], list_nama[6], list_nama[-3])
 list_nama[3] = "Aji"
 list_nama[5] = "Wanda"
 list_nama[9] = "Hani"
